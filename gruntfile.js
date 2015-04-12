8ed08e36d956bf786d9d67a75645c567e6a8ff39
Erik Reagan <erik@focuslabllc.com>
2015-04-12 13:06:24 -0400

    Commenting is courteous

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,10 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		includes: {
+			// Ahh, the include makes things much prettier and
+			// easier to work on. This is basically just for
+			// convenience and restricting changes to single
+			// files while working on the template.
 			files: {
 				src: ['build/*.html'],
 				dest: './',
@@ -17,6 +21,9 @@ module.exports = function(grunt) {
 			}
 		},
 		concat: {
+			// Combining our fieldType code samples into a single "include"
+			// for the final cheatsheet.html twig template
+			// Separating the fieltTypes by an <hr/> for now
 			fields: {
 				src: ['build/fieldTypes/*.html'],
 				dest: 'build/includes/fieldTypes.inc.html',
@@ -24,6 +31,9 @@ module.exports = function(grunt) {
 					separator: '\n\n\t<hr/>\n\n\n'
 				}
 			},
+			// We'l have a few js files to keep dev change sane
+			// so we're just gonna concatenate them by 2 line breaks
+			// for the include within cheatsheet.html
 			js: {
 				src: ['build/js/ga.js', 'build/js/core.js'],
 				dest: 'build/includes/scripts.inc.js',
@@ -48,6 +58,12 @@ module.exports = function(grunt) {
 			}
 		},
 		replace: {
+			// within the fieldType .html files we have code blocks
+			// in twig format to note certain details while working
+			// on the sample code. I don't this in the final
+			// cheatsheet.html file though, so we strip them out
+			// Hopefully no one jacks up the format and misses my
+			// pattern matching so beautifully written below.
 			comments: {
 				src: ['build/includes/fieldTypes.inc.html'],
 				overwrite: true,
