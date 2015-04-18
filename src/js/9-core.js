84f1cc7553b30ac6c70a254ab29ddc4199eb61dc
Erik Reagan <erik@focuslabllc.com>
2015-04-18 09:46:01 -0400

    Many js changes
    - Started grouping variables, methods, etc into a new cheat sheet object (GameGenie).
    - Tweaked the header so the input radio "checked" status would remain in state while using the site
    - Stripped out some html and moved it into js so if js errors occur (like jQuery not loading) the user doesn't see any elements they can't actually use
    - Moved to using external jQuery and Prism rather than the source being in the foot of the html (maybe temporary)
    
    Closes #6

--- src/js/9-core.js
+++ src/js/9-core.js
@@ -1,57 +1,102 @@
-var search, name, type, whichData, navInput;
+/*{# ---------------------------------------------
+   Remember that twig tags within the JS below
+   will be parsed by twig before the DOM reads
+   and runs the JS. Keep an eye out for:
 
-var checkForNoCode = function(selector) {
-	if ($(selector + ':visible').length === 0) {
-		console.log('TODO: add in element showing there are no results based on the search');
-	} else {
-		// console.log('TODO: add in logic to hide element when search returns results again');
-	}
-};
+   {{ }} sets
+   {% %} sets
+--------------------------------------------- #}*/
 
-var checkForNoNav = function(selector) {
-	// navInput = selector.parents('.nav_group');
-	// if (navInput + ':visible').length === 0) {
-		// console.log('TODO: add in element showing there are no results based on the search');
-	// } else {
-		// console.log('TODO: add in logic to hide element when search returns results again');
-	// }
-};
+GameGenie = {
 
-var filter = function(type, find) {
+	currentContext: '{{ sampleTag }}', // this one gets parsed by Twig later
+	newContext: null,
 
-	switch(type) {
-		case 'field':
-			whichData = 'field-handle';
-			break;
+	whichFilter:    '',
+	codeBlockCount: $('.field_block').length,
+	fieldNavCount:  $('li.field').length,
+	customElements: [],
 
-		case 'group':
-			whichData = 'group';
-			break;
+	pageSetup: function() {
 
-		default:
-			whichData = 'field-name';
-	}
+		// On read/load, focus on the filter input so a person
+		// can just start typing to find something specfic
+		$('<input type="text" value="" id="field-filter" placeholder="Find a field...">')
+			.hide()
+			.css('opacity', 0)
+			.insertBefore('#groups_list')
+			.slideDown(100, function(){
+				$(this).animate({ 'opacity': 1}, 300)
+				.focus();
+			});
 
-	// Filter content area
-	$('.field_group').each(function(i){
-		name = $(this).attr('data-' + whichData).toLowerCase();
-		if (name.indexOf(find) === -1) {
-			$(this).fadeOut('fast', checkForNoCode('.field_group'));
+		$('<div class="copy"><span class="copy_label">Click to Copy</span><svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="19" height="22" viewBox="0 0 19 22"><defs><style>.cls-2 { fill: #9f9f9f; }</style></defs><path d="M16.973 19s-.95 0-.95 0S16 18 16 18s1 0 1 0c.67 0 1-.325 1-1 0 0 0-15 0-15 0-.675-.33-1-1-1C17 1 5 1 5 1c-.67 0-1 .325-1 1 0 0 0 1 0 1s9.973 0 9.973 0C15.09 3 16 3.916 16 5.04c0 0 0 14.92 0 14.92 0 1.125-.91 2.04-2.027 2.04 0 0-11.946 0-11.946 0C.91 22 0 21.085 0 19.96c0 0 0-14.92 0-14.92C0 3.917.91 3 2.027 3 2.027 3 3 3 3 3s0-.96 0-.96C3 .917 3.91 0 5.027 0c0 0 11.946 0 11.946 0C18.09 0 19 .916 19 2.04c0 0 0 14.92 0 14.92 0 1.125-.91 2.04-2.027 2.04zM2 4c-.67 0-1 .325-1 1 0 0 0 15 0 15 0 .675.33 1 1 1 0 0 12 0 12 0 .67 0 1-.325 1-1 0 0 0-15 0-15 0-.675-.33-1-1-1C14 4 2 4 2 4zm8.454 11.01s-.573-.526-.573-.526 2.146-1.975 2.146-1.975-2.145-1.977-2.145-1.977.574-.528.574-.528 2.432 2.24 2.432 2.24c.16.145.16.38 0 .527 0 0-2.432 2.24-2.432 2.24zm-4.914 0s-2.433-2.238-2.433-2.238c-.158-.146-.158-.382 0-.527 0 0 2.433-2.24 2.433-2.24s.573.528.573.528-2.146 1.976-2.146 1.976 2.146 1.974 2.146 1.974-.573.527-.573.527z" id="path-1" class="cls-2" fill-rule="evenodd"/></svg></div>')
+		.hide()
+		.css('opacity', 0)
+		.prependTo('.code_block .code')
+		.fadeIn(100, function(){
+			$(this).animate({ 'opacity': 1}, 500);
+		});
+	},
+
+	checkForNoCode: function() {
+		if ($('.field_block').filter(':visible').length === 0) {
+			// console.log('TODO: add in element showing there are no results based on the search');
 		} else {
-			$(this).fadeIn('fast', checkForNoCode('.field_group'));
+			// console.log('TODO: add in logic to hide element when search returns results again');
 		}
-	});
+	},
+
+	checkForNoNav: function() {
+		$('.group_list').each(function() {
+			var visibleCount = $(this).find('li').filter(':visible').length;
+			groupInput = $(this).parents('.nav_group').find('input');
+			if (visibleCount === 0 || visibleCount === undefined) {
+				groupInput.prop('checked', false);
+			} else {
+				groupInput.prop('checked', true);
+			}
+		});
+	},
+
+	supaFilter: function(filterType, find) {
+
+		switch(filterType) {
+			case 'field':
+				this.whichFilter = 'field-handle';
+				break;
+
+			case 'group':
+				this.whichFilter = 'group';
+				break;
+
+			default:
+				this.whichFilter = 'field-name';
+		}
 
 
-	// Show/hide nav sections
-	$('#group_list li.field').each(function(){
-		name = $(this).attr('data-' + whichData).toLowerCase();
-		if (name.indexOf(find) === -1) {
-			$(this).fadeOut('fast', checkForNoNav($(this)));
-		} else {
-			$(this).fadeIn('fast', checkForNoNav($(this)));
-		}
-	});
+		// Filter content area
+		$('.field_block').each(function() {
+			var name = $(this).attr('data-' + GameGenie.whichFilter).toLowerCase();
+			if (name.indexOf(find) === -1) {
+				$(this).fadeOut('fast', GameGenie.checkForNoCode('.field_block'));
+			} else if ( ! $(this).is('visible')) {
+				$(this).fadeIn('fast', GameGenie.checkForNoCode('.field_block'));
+			}
+		});
+
+
+		// Show/hide nav sections
+		$('#groups_list li.field').each(function(){
+			var name = $(this).attr('data-' + GameGenie.whichFilter).toLowerCase();
+			if (name.indexOf(find) === -1) {
+				$(this).hide('fast', GameGenie.checkForNoNav);
+			} else if ($(this).is('visible') === false) {
+				$(this).show('fast', GameGenie.checkForNoNav);
+			}
+		});
+
+	},
 
 };
 
@@ -59,15 +104,17 @@ var filter = function(type, find) {
 
 $(function(){
 
-	// TODO: Add in the input field with jQuery so it isn't there for people
-	// who may have hit javascript bugs or could load jQuery for some reason
-	// $('#contact_list').find('h2').after('<input id="filterCards" type="text" value="" placeholder="Search Current Contacts">');
+	var type, find, search;
+
+	// $('<input type="text" value="" id="field-filter" placeholder="Find a field...">').insertBefore('#groups_list');
+
+	window.setTimeout(GameGenie.pageSetup, 500);
 
 	$('body').on('keyup', '#field-filter', function(){
 
-		search = $(this).prop('value').toLowerCase().replace(' ', '').split(':');
+		var search = $(this).prop('value').toLowerCase().replace(/\s/g, '').split(':');
 
-		if (search.length === 1) {
+		if (search.length === 1){
 			type = 'field';
 			find = search[0];
 		} else {
@@ -75,15 +122,65 @@ $(function(){
 			find = search[1];
 		}
 
-		filter(type, find);
+		GameGenie.supaFilter(type, find);
 
+	});
+
+
+	// Since Andy got soooo clever and used Radio buttons for the nav
+	// element switching we need some clever js to assign literal
+	// checked values. For some reason radio elements don't update
+	// the .prop('checked') value in the DOM so we can't set or check
+	// it dependably.
+	$('ul.types input[type=radio]').click(function(){
+		if ($(this).attr('checked') == 'checked') {
+			return;
+		} else {
+			$(this).parents('.types').find('input[checked="checked"]').attr('checked', null);
+			$(this).attr('checked', 'checked');
+		}
+	});
+
+
+	// New Custom Element Creation
+	$('#newCustomElement').submit(function(e){
+		e.preventDefault();
+
+		// add to the customElement array
+		var inputField = $(this).find('input[type=text]');
+		var element = inputField.val().replace(/\s/g, '');
+		$('#custom_element_list').append('<li class="custom_element"><a href="#" class="context" data-tag="'+element.toLowerCase()+'">'+element+'<span class="remove_custom">&times;</span></a></li>');
+		inputField.val('');
+	});
+
+
+	$('body').on('click', '.remove_custom', function(e){
+		e.preventDefault();
+		$(this).parents('.custom_element').animate({width: 0}, 'fast', function(){ $(this).remove(); });
 	});
 
 
 	// Element switching
 	$('body').on('click', 'a.context', function(e){
 		e.preventDefault();
-		console.log('clicked');
+		var currentElem = $('a.activeContext');
+
+		if ( ! $(this).attr('data-tag')) { return false; }
+
+		GameGenie.newContext = $(this).attr('data-tag');
+		if (GameGenie.newContext === GameGenie.currentContext) { return; }
+
+		{% verbatim -%} // jshint ignore:line
+		$('section.main_body').html($('section.main_body').html().replace(
+			new RegExp('([{%].*?)(' + GameGenie.currentContext + ')(.*?[%}])','g'),
+			'$1' + GameGenie.newContext +'$3'
+		));
+		{% endverbatim -%} // jshint ignore:line
+
+		$('a.context[data-tag="' + GameGenie.currentContext + '"]').removeClass('activeContext');
+		$('a.context[data-tag="' + GameGenie.newContext + '"]').addClass('activeContext');
+
+		GameGenie.currentContext = GameGenie.newContext;
 	});
 
 });
