e788759ce06265f0d4ea110838bb1153d66c96f6
Erik Reagan <erik@focuslabllc.com>
2015-04-21 09:43:13 -0400

    ga tweaks

--- src/js/ga.js
+++ src/js/ga.js
@@ -15,8 +15,10 @@
 --------------------------------------------- #}*/
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('set', 'metric1', '{{ cs.version }}');       // version
-ga('set', 'metric2', '{{ cs.groups|length }}'); // field group count
-ga('set', 'metric3', '{{ cs.fields|length }}'); // field count
 ga('create', 'UA-3994429-34', 'auto');
+ga('set', 'dimension1', '{{ cs.version }}');        // Cheat Sheet version
+ga('set', 'dimension2', '{{ cs.groups|length }}');  // Build field group count
+ga('set', 'dimension3', '{{ cs.fields|length }}');  // Build field count
+ga('set', 'dimension4', '{{ craft.app.version }}'); // Craft version
+ga('set', 'dimention5', '{{ craft.app.build }}');   // Craft build
 ga('send', 'pageview');
